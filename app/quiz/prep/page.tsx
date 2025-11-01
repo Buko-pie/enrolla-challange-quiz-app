7bf13989bacc71386c9656fa82ddf517ba447f10
Salvador Amba Jr <46850774+Buko-pie@users.noreply.github.com>
2025-11-02 03:02:58 +0800

    refactor: Remove unused cookie parsing logic from PrepPage component

--- app/quiz/prep/page.tsx
+++ app/quiz/prep/page.tsx
@@ -7,12 +7,6 @@ import { ExamineeGetResponse } from '@/app/constants/types'
 import { apiClient } from '@/app/lib/api'
 
 export default function PrepPage() {
-  const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-    const [key, value] = cookie.split('=')
-    acc[key] = value
-    return acc
-  }, {} as Record<string, string>)
-
   const [token, setToken] = useState('');
   const [loading, setLoading] = useState(true);
   const [examineeName, setExamineeName] = useState('')
